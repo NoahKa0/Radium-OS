89d897bd49b98b65fc2a23695171adeb1c63baa2
NoahKa0 <noahkastelein0@gmail.com>
2019-11-17 20:06:16 +0100

    Updated timer
    After a reset of broadcom_BCM5751, the system should wait 0,1 seconds.
    This is not possible since the timer uses interrupts and interrupts are disabled when drivers are being initialized.
    So i added a function that can sleep without interrupts.

--- include/timer.h
+++ include/timer.h
@@ -2,19 +2,33 @@
 #define __SYS__TIMER_H
 
   #include <common/types.h>
+  #include <hardware/port.h>
+  
   namespace sys {
     class SystemTimer {
     protected:
       common::uint64_t time;
+      common::uint32_t frequency;
+      
+      hardware::Port8Bit channel0;
+      hardware::Port8Bit channel1;
+      hardware::Port8Bit channel2;
+      hardware::Port8Bit command;
+      
+      common::uint16_t readTime();
     public:
       static SystemTimer* activeTimer;
       
       SystemTimer();
       ~SystemTimer();
       
+      void onTimerInterrupt();
+      
       static common::uint64_t getTimeInInterrupts();
       
-      void onTimerInterrupt();
+      static void sleep(common::uint32_t milliseconds);
+      
+      static void sleepWithIntDisabled(common::uint32_t milliseconds);
     };
   }
 
--- src/timer.cpp
+++ src/timer.cpp
@@ -11,16 +11,36 @@ using namespace sys::common;
 
 SystemTimer* SystemTimer::activeTimer = 0;
 
-SystemTimer::SystemTimer() {
+SystemTimer::SystemTimer()
+:channel0(0x40),
+channel1(0x41),
+channel2(0x42),
+command(0x43)
+{
   activeTimer = this;
   
   this->time = 0;
+  this->frequency = 18;
 }
 
 SystemTimer::~SystemTimer() {
   if(activeTimer == this) {
     activeTimer = 0;
   }
+}
+
+uint16_t SystemTimer::readTime() {
+  uint16_t ret = 0;
+  
+  this->command.write(0b00000000); // might need to be 0b11000000 we'll see.
+  ret = this->channel0.read();
+  ret |= (this->channel0.read() << 8) & 0xFF00;
+  
+  return ret;
+}
+
+void SystemTimer::onTimerInterrupt() {
+  this->time++;
 }
 
 uint64_t SystemTimer::getTimeInInterrupts() {
@@ -30,6 +50,33 @@ uint64_t SystemTimer::getTimeInInterrupts() {
   return 0;
 }
 
-void SystemTimer::onTimerInterrupt() {
-  this->time++;
+void SystemTimer::sleep(uint32_t milliseconds) {
+  if(SystemTimer::activeTimer == 0) return;
+  
+  uint32_t sleepTime = (milliseconds*SystemTimer::activeTimer->frequency)/1000;
+  uint32_t currentTime = SystemTimer::getTimeInInterrupts();
+  while(SystemTimer::getTimeInInterrupts() < currentTime+sleepTime) {
+    asm("hlt");
+  }
+}
+
+void SystemTimer::sleepWithIntDisabled(uint32_t milliseconds) {
+  if(SystemTimer::activeTimer == 0) return;
+  
+  SystemTimer* timer = SystemTimer::activeTimer;
+  // PIT runs at 1193181 herz, since we want milliseconds, divide by 1000.
+  uint64_t sleepTime = (milliseconds*1193);
+  uint64_t expired = 0;
+  uint16_t currentRead = ~timer->readTime(); // Invert so it ascents.
+  uint16_t newRead = 0;
+  
+  while(expired <= sleepTime) {
+    newRead = ~timer->readTime(); // Invert so it ascents.
+    if(newRead < currentRead) {
+      expired += newRead + (~currentRead & 0xFFFF); // When newRead is smaller there was a wrap around. So invert currentRead so i get the number until max.
+    } else {
+      expired += (newRead - currentRead);
+    }
+    currentRead = newRead;
+  }
 }
